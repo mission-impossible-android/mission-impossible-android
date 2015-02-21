ee5228dac87c9b5b06f473662052a48f7486965e
Mike Perry <mikeperry-git@torproject.org>
2015-02-20 20:53:37 -0800

    Add more useful free APKs since MyAppList doesn't suport F-Droid :(
    It is a hassle to include these, mostly due to versioning, but unfortunately
    the MyAppList app only supports Google Play, so it is mostly useless to people
    who want a decent experience without GApps.

--- scripts/download_apks.sh
+++ scripts/download_apks.sh
@@ -2,6 +2,10 @@
 
 BASEDIR=$(readlink -e pkg)
 
+# FIXME: Can we get a better interface from F-Droid to get the latest apk
+# version? Maybe just a symlink on their side? It's either that, or we
+# write some sketch regex scraper for app URLs, which are regular
+# (ex: https://f-droid.org/repository/browse/?fdid=org.ethack.orwall)
 apk_files=( \
   org.torproject.android_133.apk \
   org.ethack.orwall_35.apk \
@@ -11,6 +15,18 @@ apk_files=( \
   net.osmand.plus_197.apk \
   com.csipsimple_2417.apk \
   com.projectsexception.myapplist.open_16.apk \
+  fr.kwiatkowski.ApkTrack_1.apk \
+  org.coolreader_875.apk \
+  com.artifex.mupdfdemo_55.apk \
+  org.mozilla.fennec_fdroid_350010.apk \
+  fennec-35.0.multi.android-arm.apk \
+  de.schildbach.wallet_209.apk \
+  org.mariotaku.twidere_98.apk \
+  com.morlunk.mumbleclient_72.apk \
+  indrora.atomic_19.apk \
+  com.xabber.androiddev_120.apk \
+  org.videolan.vlc_10104.apk \
+  org.sufficientlysecure.localcalendar_6.apk
 )
 
 echo "--- Downloading F-Droid app market APK..."
