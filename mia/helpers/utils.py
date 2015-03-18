29e9430972930bd33af005f540ca4ccc42388927
Schneider Werner-Walter <noreply@schnwalter.eu>
2015-03-18 11:42:21 +0200

    Add basic util functions.

--- mia/helpers/utils.py
+++ mia/helpers/utils.py
@@ -42,3 +42,73 @@ class MiaHandler(metaclass=Singleton):
         else:
             logging.error(msg)
 
+
+def input_confirm(display_text='Confirm', default_value=False):
+    """
+    Ask the user to confirm an action.
+    :param display_text:
+    :param default_value:
+    :return: boolean
+    """
+
+    # Update the text depending on the default return value.
+    if default_value:
+        display_text = '%s [%s/%s]: ' % (display_text, 'Y', 'n')
+    else:
+        display_text = '%s [%s/%s]: ' % (display_text, 'y', 'N')
+
+    while True:
+        value = input(display_text)
+        value = value.lower()
+
+        # Return the default value.
+        if not value:
+            return default_value
+
+        if value not in ['y', 'yes', 'n', 'no']:
+            print('This is a yes and no question!')
+            continue
+
+        if value == 'y' or value == 'yes':
+            return True
+        if value == 'n' or value == 'no':
+            return False
+
+
+def input_ask(display_text, default_value=None, free_text=False):
+    """
+    Ask the user to provide a string.
+    :param display_text:
+    :param default_value:
+    :return: string
+    """
+
+    print("free_text: %s" % free_text)
+    # Update the text depending on the default return value.
+    if default_value:
+        display_text = '%s [%s]: ' % (display_text, default_value)
+    else:
+        display_text = '%s: ' % display_text
+
+    while True:
+        value = input(display_text)
+
+        # Return the default value, if any.
+        if not value and default_value is not None:
+            return default_value
+        elif not value:
+            continue
+
+        # Limit the allowed characters.
+        if not free_text:
+            import re
+            if not re.search(r'^[a-z][a-z0-9-]+$', value):
+                print('A sting containing letters, numbers, hyphens.')
+                print('The string must start with a letter.')
+                continue
+
+        return value
+
+
+def print_nl(string=''):
+    print(string + "\n")
