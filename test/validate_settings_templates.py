9be51d4f140152db0ba41e9147d4df91c39f155c
Patrick Connolly <patrick.c.connolly@gmail.com>
2015-06-17 13:40:59 -0400

    Check each template schema during testing. Check only current sample schema. [#97]

--- test/validate_settings_templates.py
+++ test/validate_settings_templates.py
@@ -1,9 +1,15 @@
+from glob import glob
+import sys
 import yamale
 
 schema = yamale.make_schema('./docs/schema.yaml')
 
-data = yamale.make_data('./docs/target.settings.yaml')
+data = yamale.make_data('./docs/current.settings.yaml')
 yamale.validate(schema, data)
 
-data = yamale.make_data('./mia/templates/mia-default/settings.yaml')
-yamale.validate(schema, data)
+templates = glob('mia/templates/*/settings.yaml')
+for template in templates:
+    sys.stdout.write('Checking %s against schema... ' % template)
+    data = yamale.make_data(template)
+    yamale.validate(schema, data)
+    print("done!")
