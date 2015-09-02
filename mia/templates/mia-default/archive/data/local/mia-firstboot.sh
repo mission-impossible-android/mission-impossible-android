2e9066913f37e4e985b115a06dea873d1920b7be
Schneider Werner-Walter <noreply@schnwalter.eu>
2015-09-02 04:38:16 +0300

    Update firstboot logs.

--- mia/templates/mia-default/archive/data/local/mia-firstboot.sh
+++ mia/templates/mia-default/archive/data/local/mia-firstboot.sh
@@ -50,6 +50,7 @@ SIP_UID=$(get_app_uid $SIP_APP)
 ORWALL_UID=$(get_app_uid $ORWALL_APP)
 
 # Fix orwall's config UIDs
+fb_logger "Updating UIDs in the orwall preferences file."
 $SED -i /data/data/org.ethack.orwall/shared_prefs/org.ethack.orwall_preferences.xml -e "s/REPLACE_WITH_BROWSER_UID/$BROWSER_UID/"
 $SED -i /data/data/org.ethack.orwall/shared_prefs/org.ethack.orwall_preferences.xml -e "s/REPLACE_WITH_SIP_UID/$SIP_UID/"
 
@@ -59,16 +60,22 @@ mkdir -p /data/data/com.android.settings/databases
 $SED -i $MISC_DIR/com.android.settings_su.sql -e "s/REPLACE_WITH_ORWALL_UID/$ORWALL_UID/"
 /system/xbin/sqlite3 /data/data/com.android.settings/databases/su.sqlite < $MISC_DIR/com.android.settings_su.sql
 
-# Generate SQL to create orWall app DB
+ORWALL_APP_DATA="${MISC_DIR}/app-list-orwall.txt"
+fb_logger "Generating SQL to populate the orWall NAT database."
+fb_logger "Using app data from file: ${ORWALL_APP_DATA}."
 SQL_FRAGMENT=""
 while read APP_DATA; do
   APP_NAME=$(echo $APP_DATA | $CUT -f'1' -d':')
   APP_UID=$(get_app_uid $APP_NAME)
+  fb_logger "- preparing SQL for ${APP_NAME}; UID: ${APP_UID}."
+
   APP_PORT_TYPE=$(echo $APP_DATA | $CUT -f'2' -d':')
   APP_PORT_TYPE=${APP_PORT_TYPE:-TransProxy} # Default port type
   SQL_LINE="INSERT INTO rules VALUES($APP_UID,'$APP_NAME','Tor',9040,'$APP_PORT_TYPE');"
   SQL_FRAGMENT="$SQL_LINE\n$SQL_FRAGMENT"
-done < $MISC_DIR/app-list-orwall.txt
+done < ${ORWALL_APP_DATA}
+
+fb_logger "Updating the orWall NAT database."
 $SED -i $MISC_DIR/org.ethack.orwall_nat.sql -e "s/{{REPLACE_WITH_GENERATED_SQL}}/$SQL_FRAGMENT/"
 mkdir -p /data/data/org.ethack.orwall/databases
 /system/xbin/sqlite3 /data/data/org.ethack.orwall/databases/nat.s3db < $MISC_DIR/org.ethack.orwall_nat.sql
@@ -78,27 +85,32 @@ for APP in ${APPS[@]}
 do
   PKG_LINE=$( $CAT /data/system/packages.xml | $EGREP "^[ ]*<package.*serId" | $GREP -v framework-res.apk | $GREP -v com.htc.resources.apk | $GREP -i $APP )
   APP_UID=$( $ECHO $PKG_LINE | $SED 's%.*serId="\(.*\)".*%\1%' |  $CUT -d '"' -f1)
-  fb_logger "Fixup for app $APP uid $APP_UID."
+  fb_logger "Fix the ${APP} application data owner and group; UID: $APP_UID."
   $FIND /data/data/$APP -type d -exec $CHOWN $APP_UID:$APP_UID {} \;
   $FIND /data/data/$APP -type d -exec $CHMOD 0771 {} \;
   $FIND /data/data/$APP -type f -exec $CHOWN $APP_UID:$APP_UID {} \;
   $FIND /data/data/$APP -type f -exec $CHMOD 0660 {} \;
 done
 
-# For some reason, these settings are ignored if run from the updater-script,
-# so we set them here:
+fb_logger "Settings.Secure.ADB_ENABLED -- Disable ADB by default."
+# NOTE: This does not prevent the sytem from asking to authorize the connection.
 /system/bin/settings put global adb_enabled 0
+
+fb_logger "Settings.Secure.LOCATION_PROVIDERS_ALLOWED -- Limit the list of location providers that activities may access."
 /system/bin/settings put secure location_providers_allowed ""
+
+fb_logger "Settings.Global.AUTO_TIME -- Do not update the date, time and time zone automatically from the network (NITZ)."
 /system/bin/settings put global auto_time 0
 
 # GH-94: Enable Airplane Mode and broadcast the event.
+fb_logger "Settings.Global.AIRPLANE_MODE_ON -- Enable Airplane Mode and broadcast the event."
 /system/bin/settings put global airplane_mode_on 1
 /system/bin/am broadcast -a android.intent.action.AIRPLANE_MODE --ez state true
 
-# Change hostname for subsequent reboots
+fb_logger "Change hostname for subsequent reboots."
 /system/bin/settings put secure device_hostname "localhost"
 
-# Change hostname for current boot
+fb_logger "Change hostname for current boot."
 setprop net.hostname localhost
 echo localhost > /proc/sys/kernel/hostname
 
@@ -114,7 +126,7 @@ am startservice org.torproject.android/org.torproject.android.service.TorService
 # TODO: Investigate why this doesn't happen on its own.
 am broadcast -a android.intent.action.BOOT_COMPLETED -n org.ethack.orwall/.BootBroadcast
 
-# Remove setup MISC_DIR
+fb_logger "Remove setup directory: ${MISC_DIR}"
 rm -rf $MISC_DIR
 
 
